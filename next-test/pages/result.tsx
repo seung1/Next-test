9e61478fcc8737f2fd8e0e03b2f824241e570db4
seung1 <97tmddnjs@naver.com>
2023-03-14 22:53:02 +0900

    Add page test

---
+++ next-test/pages/result.tsx
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { ReactElement } from 'react';
+
+export default function Result() {
+    return (
+        <>
+            <div>result page</div>
+            <div>result page</div>
+            <div>result page</div>
+        </>
+    );
+};
