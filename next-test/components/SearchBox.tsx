e3c421e11c514920d46476dc3a266501fa5ecdea
seung1 <97tmddnjs@naver.com>
2023-04-09 18:26:45 +0900

    feat: Apply code refactoring and add list detail

--- next-test/components/SearchBox.tsx
+++ next-test/components/SearchBox.tsx
@@ -1,65 +1,260 @@
-import { TextFields } from '@mui/icons-material';
-import { Box, Button, Chip, ChipProps, Divider, Grid, IconButton, InputBase, ListItem, ListItemButton, ListItemText, Paper, TextField, Typography } from '@mui/material';
-import { useState } from 'react';
-import SearchIcon from '@mui/icons-material/Search';
+import { TextFields } from "@mui/icons-material";
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Chip,
+  ChipProps,
+  Divider,
+  Grid,
+  IconButton,
+  InputBase,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState, KeyboardEvent } from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBox = () => {
-    const [searchResult, setSearchResult] = useState(['검색조건1', '검색조건2', '검색조건3']);
-    const testData = Array.from({ length: 20 }, (_, index) => index + 1);
-    return (<>
-        <Box sx={{
-            backgroundColor: '#B6C5B8',
-        }}>
-            <Grid sx={{ my: 4 }}>
-                <Paper
-                    component="form"
-                    sx={{
-                        p: '2px 4px',
-                        display: 'flex',
-                        alignItems: 'center',
-                        // boxShadow: 'none'
-                    }}
-                >
-                    <InputBase
-                        sx={{ ml: 1, flex: 1 }}
-                    />
-                    <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                        <SearchIcon />
-                    </IconButton>
-                </Paper>
-            </Grid>
-            <Grid >
-                <Box sx={{ justifyContent: 'center' }}>
-                    <Box sx={{
-                        width: { xs: '90vw', md: '42vw' },
-                        height: { xs: '75vh', md: '80vh' },
-                        overflow: 'auto',
-                        backgroundColor: "white",
-                        borderRadius: 3,
-                        p: 2,
-                        '&::-webkit-scrollbar': {
-                            width: '12px',
-                            backgroundColor: 'lightgray'
-                        },
-                        '&::-webkit-scrollbar-thumb': {
-                            backgroundColor: 'gray',  // 스크롤바 색상
-                            borderRadius: '20px',  // 스크롤바 모양
-                        },
-                    }}>
-                        {testData.map((item, index) => (
-                            <ListItem key={index} sx={{ py: 0 }}>
-                                <ListItemButton>
-                                    <ListItemText primary={`Item ${item}`} />
-                                </ListItemButton>
-                            </ListItem>
-                        ))}
+  const [searchResult, setSearchResult] = useState([
+    "검색조건1",
+    "검색조건2",
+    "검색조건3",
+  ]);
+  const getTestData = () => [
+    {
+      title: "드로낙 리뷰) 드로낙이라는 걸 처음 먹어보는데 리뷰한번 해봄",
+      recommendCount: 235,
+      commentCount: 312,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 255,
+      commentCount: 1323,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 12,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 122,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 35,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 300,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+    {
+      title: "드로낙 리뷰",
+      recommendCount: 2,
+      commentCount: 3,
+      createdAt: "2023-04-04",
+    },
+  ];
+  const [searchInput, setSearchInput] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
-                    </Box>
-                </Box>
-            </Grid>
+  const enterKeyEventOnSearch = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const target = e.target as HTMLInputElement;
+      target.blur();
+      setSearchQuery(searchInput);
+    }
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: "#B6C5B8",
+        }}
+      >
+        <Box sx={{ my: 4 }}>
+          <Paper
+            component="form"
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              // boxShadow: 'none'
+            }}
+          >
+            <InputBase
+              type="search"
+              placeholder="검색어를 입력하세요."
+              sx={{ ml: 1, flex: 1 }}
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              onKeyPress={enterKeyEventOnSearch}
+            />
+            <IconButton
+              type="button"
+              sx={{ p: "10px" }}
+              aria-label="search"
+              onClick={() => setSearchQuery(searchInput)}
+            >
+              <SearchIcon />
+            </IconButton>
+          </Paper>
         </Box>
+        <Box>{` "${searchQuery}" 에 대한 검색 결과`}</Box>
+        <Box>
+          <Box
+            sx={{
+              backgroundColor: "white",
+              borderRadius: 3,
+              width: { xs: "90vw", md: "42vw" },
+              height: { xs: "70vh", md: "78vh" },
+            }}
+          >
+            <Box
+              id="list label"
+              sx={{
+                display: "flex",
+                fontWeight: 700,
+                gap: 1,
+                p: "8px 16px 0",
+                width: "100%",
+              }}
+            >
+              <Box sx={{ flexGrow: 1 }}>제목</Box>
+              <Box sx={{ minWidth: "40px" }}>추천수</Box>
+              <Box sx={{ minWidth: "40px" }}>댓글수</Box>
+              <Box sx={{ minWidth: "85px", whiteSpace: "nowrap" }}>작성일</Box>
+            </Box>
+            <Box
+              sx={{
+                // width: { xs: '90vw', md: '42vw' },
+                height: { xs: "65vh", md: "75vh" },
+                overflow: "auto",
+                // backgroundColor: "white",
+                // borderRadius: 3,
+                position: "relative",
+                p: "6px 12px 12px",
+                "&::-webkit-scrollbar": {
+                  width: "12px",
+                  backgroundColor: "lightgray",
+                },
+                "&::-webkit-scrollbar-thumb": {
+                  backgroundColor: "gray", // 스크롤바 색상
+                  borderRadius: "20px", // 스크롤바 모양
+                },
+              }}
+            >
+              {[...getTestData()].map((item, index) => (
+                <>
+                  <ListItemButton
+                    key={index}
+                    title={item.title}
+                    sx={{ gap: 1, px: 0 }}
+                  >
+                    <Box
+                      sx={{
+                        display: "-webkit-box",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        WebkitLineClamp: 2,
+                        WebkitBoxOrient: "vertical",
+                        overflowWrap: "anywhere",
+                        flexGrow: 1,
+                      }}
+                    >
+                      {item.title}
+                    </Box>
+                    <Box sx={{ minWidth: "40px" }}>{item.recommendCount}</Box>
+                    <Box sx={{ minWidth: "40px" }}>{item.commentCount}</Box>
+                    <Box sx={{ minWidth: "85px", whiteSpace: "nowrap" }}>
+                      {item.createdAt}
+                    </Box>
+                  </ListItemButton>
+                  <Divider />
+                </>
+              ))}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
     </>
-    );
+  );
 };
 
 export default SearchBox;
